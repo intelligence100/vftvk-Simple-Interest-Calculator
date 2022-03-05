8d0435b759d228d9f19d697ac43e46c168acfe62
intelligence100 <r.bhagates@gmail.com>
2022-03-05 17:35:06 +0000

    changes made from theia lab environment

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
-function compute()
+var principal = document.getElementById("principal").value;
+var rate = document.getElementById("rate").value;
+var years = document.getElementById("years").value;
+var intrest = principal * years * rate/100;
+var year = new Date().getFullYear()+parseInt(years);
+
+document.getElementById("rate").onchange = function(){updateRate()}
+function updateRate()
 {
-    p = document.getElementById("principal").value;
-    
+   var rateval =document.getElementById("rate").value;
+   document.getElementById("rate_val").innerText=rateval;    
 }
-        
